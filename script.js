49e39e237616baaf207950fa3fe8bb552c33861d
Emil-Ant <emiliosantoun411@gmail.com>
2022-05-14 16:59:31 +0300

    Added user play

--- script.js
+++ script.js
@@ -2,12 +2,29 @@ const Choices = ["Rock", "Paper", "Scissors"];
 
 function computerPlays(Choices) {
   //generate random index from the array 
-  const randomIndex = [Math.floor(Math.random() * Choices.length)]; 
+  const computerRandomIndex = [Math.floor(Math.random() * Choices.length)]; 
   //retrieve item that corresponds to the generated index 
-  const item = Choices[randomIndex]; 
+  const computerItem = Choices[computerRandomIndex]; 
 
-  return item; 
+  return computerItem; 
 
 } 
 
-const computerChoice = computerPlays(Choices); 
+function userPlays(Choices) {
+    //generate random index from the array 
+    const userRandomIndex = [Math.floor(Math.random() * Choices.length)]; 
+    //retrieve item that corresponds to the generated index 
+    const userItem = Choices[userRandomIndex]; 
+
+    return userItem; 
+  
+
+} 
+
+const computerRound1 = computerPlays(Choices); 
+const playerRound1 = userPlays(Choices); 
+
+
+
+
+
